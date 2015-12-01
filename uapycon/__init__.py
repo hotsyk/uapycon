c5e275519f39ad3fc551ea30d0a05ddfd731aedd
Volodymyr Hotsyk <gotsyk@gmail.com>
2015-12-01 07:41:48 -0800

    Get days till PyCon Ukraine 2016

--- uapycon/__init__.py
+++ uapycon/__init__.py
@@ -3,3 +3,5 @@
 __author__ = 'Volodymyr Hotsyk'
 __email__ = 'gotsyk@gmail.com'
 __version__ = '0.1.0'
+
+from .uapycon import days_till_pycon
--- uapycon/uapycon.py
+++ uapycon/uapycon.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+
+import datetime
+
+
+def days_till_pycon():
+    today = datetime.date.today()
+    pycon_start = datetime.date(2016, 4, 23)
+    if pycon_start > today:
+        return "PyCon Ukraine starts in {0} days".format(
+            (pycon_start - today).days)
+    elif pycon_start == today:
+        return "Hurry up, PyCon Ukraine is today!"
+    else:
+        return "Looks, like you are missing it :("
